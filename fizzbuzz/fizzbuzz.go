73d29e1a8b030cbc600d513110778ebdb7e691a2
Curtis Ault <curtisault@gmail.com>
2017-03-14 12:02:26 -0500

    initial commit for fizzbuzz

---
+++ fizzbuzz/fizzbuzz.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	for i := 1; i <= 25; i++ {
+		printType := 0
+
+		if i%15 == 0 {
+			printType = 15
+		} else if i%5 == 0 {
+			printType = 5
+		} else if i%3 == 0 {
+			printType = 3
+		}
+
+		switch printType {
+		case 3:
+			fmt.Println("Fizzles")
+		case 5:
+			fmt.Println("Buzzles")
+		case 15:
+			fmt.Println("FizzBuzzles")
+		default:
+			fmt.Println(i)
+		}
+	}
+}
